fbc1ffdbfbaae1840d67338425f801c5814e79da
Elliot Robinson <elliot.robinson@argiopetech.com>
2014-10-31 23:03:45 -0400

    Initial commit

---
+++ Text/Ordinal.hs
@@ -0,0 +1,33 @@
+{- |
+Module      :  Text.Ordinal
+Copyright   :  (c) 2014 Elliot Robinson
+               (c) 2014 Argiope Technical Solutions
+License     :  BSD3
+
+Maintainer  :  Elliot Robinson <elliot.robinson@argiopetech.com>
+Stability   :  experimental
+Portability :  portable
+
+Provides conversion from Integral types to 
+-}
+
+module Text.Ordinal where
+
+import Data.Text.Lazy             (Text(), pack, append)
+import Data.Text.Lazy.Builder     (toLazyText)
+import Data.Text.Lazy.Builder.Int (decimal)
+
+showOrdinal :: Integral a => a -> Text
+showOrdinal i = let pref = toLazyText $ decimal i
+                    suff = ordinalSuffix i
+                in pref `append` suff
+
+ordinalSuffix :: Integral a => a -> Text
+ordinalSuffix i = let i' = (abs i) `mod` 100
+                  in if i' > 10 && i' < 20
+                       then pack "th"
+                       else pack $ go $ i' `mod` 10
+    where go 1  = "st"
+          go 2  = "nd"
+          go 3  = "rd"
+          go _  = "th"
